8c2aa2317dbc8a2f97a92e5963b9ca0d6b89e6d3
Jennifer Wjertzoch <jennifer.wjertzoch@oetkerdigital.com>
2020-06-04 09:07:29 +0200

    Improve javascript file

--- js/script.js
+++ js/script.js
@@ -1,35 +1,33 @@
 'use strict';
 
-var signupBtn = document.querySelector('[data-element=main-signupbtn]');
-var closeBtn = document.querySelector('[data-element=close]');
-var cancelBtn = document.querySelector('[data-element=cancelbtn]')
-var modal = document.querySelector('[data-element=main-modal]');
-var modalContent = document.querySelector('[data-element=modal-content]');
-
 const PersonalBlog = (function() {
+    const signupBtn = document.querySelector('[data-element=main-signupbtn]')
+    const closeBtn = document.querySelector('[data-element=close]')
+    const cancelBtn = document.querySelector('[data-element=cancelbtn]')
+    const modal = document.querySelector('[data-element=main-modal]')
 
-    const signUpModal = () => {
-        function openModal() {
-            modal.style.display = 'block';
-            modalContent.style.display = 'block';
-        }
+    const openModal = function() {
+        modal.style.display = 'block'
+    }
 
-        function closeModal() {
-            modal.style.display = 'none';
-        }
+    const closeModal = function() {
+        modal.style.display = 'none'
+    }
 
-        signupBtn.onclick = openModal;
-        closeBtn.onclick = closeModal;
-        cancelBtn.onclick = closeModal;
+    const buttonAction = function() {
+        signupBtn.onclick = openModal
+        closeBtn.onclick = closeModal
+        cancelBtn.onclick = closeModal
     }
 
     const init = function() {
-        signUpModal();
-    };
+        buttonAction()
+    }
 
     return {
         init: init,
-    };
-})();
+    }
 
-document.addEventListener('DOMContentLoaded', PersonalBlog.init);
+  })()
+
+document.addEventListener('DOMContentLoaded', PersonalBlog.init)
